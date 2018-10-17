aa9a13268165edd1787db276b2af9802e4cf9a81
Dilz <dilz@usmonov.com>
2018-10-17 20:10:47 +0200

    Init. Pod added.

---
+++ Picsumple/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Picsumple
+//
+//  Created by Dilz Osmani on 17/10/2018.
+//  Copyright © 2018 com.picsumpl.test. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
